f1f33a5a4bbb807020546ac1588ed0aaeb54bfc0
Will Minshew <wminshew@gmail.com>
2023-03-06 19:15:44 -0800

    format script

--- script/UniV3OracleData.s.sol
+++ script/UniV3OracleData.s.sol
@@ -11,41 +11,25 @@ import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
 contract UniV3OracleDataScript is Script {
-        address[3] tokens = [
-                                    0x6B175474E89094C44Da98b954EedeAC495271d0F, // DAI
-                                    0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, // USDC
-                                    0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 // WETH
-                             ];
+    address[3] tokens = [
+        0x6B175474E89094C44Da98b954EedeAC495271d0F, // DAI
+        0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, // USDC
+        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 // WETH
+    ];
 
-        string[3] tokensStr = [
-                                      "DAI", // DAI
-                                      "USDC", // USDC
-                                      "WETH" // WETH
-                                    ];
+    string[3] tokensStr = [
+        "DAI", // DAI
+        "USDC", // USDC
+        "WETH" // WETH
+    ];
 
-        uint24[4] fees = [
-                          uint24(1_00),
-                          uint24(5_00),
-                          uint24(30_00),
-                          uint24(100_00)
-                          ];
+    uint24[4] fees = [uint24(1_00), uint24(5_00), uint24(30_00), uint24(100_00)];
 
-        string[4] feesStr = [
-                                      "0.01%",
-                                      "0.05%",
-                                      "0.3%",
-                                      "1.0%"
-                                      ];
+    string[4] feesStr = ["0.01%", "0.05%", "0.3%", "1.0%"];
 
-        uint32[5] periods = [
-                                 uint32(1 minutes),
-                                 uint32(5 minutes),
-                                 uint32(10 minutes),
-                                 uint32(30 minutes),
-                                 uint32(1 hours)
-                                 ];
+    uint32[5] periods = [uint32(1 minutes), uint32(5 minutes), uint32(10 minutes), uint32(30 minutes), uint32(1 hours)];
 
-        uint128 constant baseAmount = 1e18;
+    uint128 constant baseAmount = 1e18;
 
     function setUp() public {}
 
@@ -65,25 +49,18 @@ contract UniV3OracleDataScript is Script {
                     console.log();
                     console.log("pool %s", pool);
                     console.log("(%s, %s, %s)", tokensStr[i], tokensStr[j], feesStr[k]);
-                    (
-                     uint160 sqrtRatioX96,
-                     int24 tick,
-                     ,
-                     uint16 observationCardinality,
-                     ,
-                     ,
-                     ) = IUniswapV3Pool(pool).slot0();
+                    (uint160 sqrtRatioX96, int24 tick,, uint16 observationCardinality,,,) = IUniswapV3Pool(pool).slot0();
 
                     console.log("tick: ");
                     console.logInt(tick);
                     console.log("sqrtRatioX96: %s", sqrtRatioX96);
 
                     uint256 quoteAmount = OracleLibrary.getQuoteAtTick({
-                                                                 tick: tick,
-                                                                 baseAmount: baseAmount,
-                                                                 baseToken: tokens[i],
-                                                                 quoteToken: tokens[j]
-                                                                 });
+                        tick: tick,
+                        baseAmount: baseAmount,
+                        baseToken: tokens[i],
+                        quoteToken: tokens[j]
+                    });
                     console.log("receive %s of %s for", quoteAmount, tokensStr[j]);
                     console.log(" %s of %s", baseAmount, tokensStr[i]);
                     console.log("num observations: ", observationCardinality);
@@ -104,7 +81,7 @@ contract UniV3OracleDataScript is Script {
                                 baseAmount: baseAmount,
                                 baseToken: tokens[i],
                                 quoteToken: tokens[j]
-                                });
+                            });
                             console.log("%s second twap", period);
                             console.log("   tick:");
                             console.logInt(arithmeticMeanTick);
@@ -114,7 +91,6 @@ contract UniV3OracleDataScript is Script {
                             console.log("- %s second twap: not enough observations", period);
                         }
                     }
-
                 }
             }
         }
